9a58d1e11d9a26afc3fd6d67ab45112add6a7d45
RaiyanKhan19-41453-3 <raiyankhan1982@gmail.com>
2022-06-08 23:18:31 +0600

    first commit

--- LabTask2/view/h2.php
+++ LabTask2/view/h2.php
@@ -1,3 +1,7 @@
+<?php
+include("../control/p2.php");
+?>
+
 <html>
     <head>
             <title>
@@ -9,21 +13,29 @@
 
         <h1>Registration Form</h1>
 
-        <form action="../control/p1.php" method="GET">
+        <form action="" method="POST" enctype="multipart/form-data">
             <table>
                 <tr>
                     <td>First Name:</td>
                     <td><input type = "text" name = "fname"></td>
+                    <td><?php
+                        echo $uvalid;
+                    ?></td>
                 </tr>
 
                 <tr>
                     <td>Last Name:</td>
-                    <td><input type = "text" name = "lname"></td>
+                    <td><input type = "text" name = "lname">
+                </td>
                 </tr>
 
                 <tr>
                     <td>Age:</td>
                     <td><input type = "number" name = "age"></td>
+                    <td>
+                    <?php
+                        echo $agevalid;
+                    ?></td>
                 </tr>
 
                 <tr>
@@ -31,6 +43,10 @@
                     <td><input type = "radio" name = "desig" value = "Junior Programmer">Junior Programmer
                     <input type = "radio" name = "desig" value = "Senior Programmer">Senior Programmer
                     <input type = "radio" name = "desig" value = "Project Lead">Project Lead</td>
+                    <td>
+                    <?php
+                        echo $desigvalid;
+                    ?></td>
                 </tr>
 
                 <tr>
@@ -38,26 +54,50 @@
                     <td><input type = "checkbox" name = "l1" value = "JAVA">JAVA
                     <input type = "checkbox" name = "l2" value = "PHP">PHP
                     <input type = "checkbox" name = "l3" value = "C++">C++</td>
+                    <td>
+                    <?php
+                        echo $langvalid;
+                        echo $langvalid1;
+                        echo $langvalid2;
+                        echo $langvalid3;
+                    ?></td>
                 </tr>
 
                 <tr>
                     <td>E-mail:</td>
                     <td><input type="email" name = "email"></td>
+                    <td>
+                    <?php
+                        echo $evalid;
+                    ?></td>
                 </tr>
 
                 <tr>
                     <td>Password:</td>
                     <td><input type="password" name = "password"></td>
+                    <td>
+                    <?php
+                        echo $pvalid;
+                    ?></td>
                 </tr>
                
                 <tr>
                     <td>Please choose a file</td>
-                    <td><input type="file" name = "file"></td>
+                    <td><input type="file" name="filetoupload">
+                </td>
+                <td>
+                    <?php
+                        echo $file_name;
+                    ?></td>
                 </tr>
 
                 <tr>
-                    <td><input type = "submit">
+                    <td><input type = "submit" name = "sub">
                     <input type="reset"></td>
+                    <td>
+                    <?php
+                        echo $dvalid;
+                    ?></td>
                 </tr>
 
             </table>
